4d93a903116011e625f5dee49dbe10b60a6d1c02
yoga21 <50547906+yoga21@users.noreply.github.com>
2020-04-15 12:45:14 +0530

    Add files via upload

---
+++ EUREKA AND DESCOVERY/EurekaRegistry/src/test/java/com/quickgrowup/com/EurekaRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quickgrowup.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
