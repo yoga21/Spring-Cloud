a8af1e83e447aa4dad4a7f58171d548704987a61
yoga21 <50547906+yoga21@users.noreply.github.com>
2020-04-30 18:22:17 +0530

    Add files via upload

---
+++ ActuatorEndPoint/src/main/java/com/oauth/demo/ConfigFileApplication.java
@@ -0,0 +1,13 @@
+package com.oauth.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigFileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigFileApplication.class, args);
+	}
+
+}
